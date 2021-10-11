5087f76d4923dfc91a09bc05ef0ad72f54c90cbe
Crowdbotics <team@crowdbotics.com>
2021-10-11 12:09:13 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/ndd_online_31313/MainActivity.java
@@ -0,0 +1,15 @@
+package com.ndd_online_31313;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "ndd_online_31313";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "ndd_online_31313",
+  url: "https://ndd_online_31313.botics.co",
+  api: "https://ndd_online_31313.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
